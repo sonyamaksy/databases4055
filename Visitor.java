8cff669d37175aa0796a67a546f52ec25c6ec12b
sonyamaksy <79486998+sonyamaksy@users.noreply.github.com>
2022-11-22 18:38:20 -0600

    Add files via upload
    the switching between scenes doesn't work and i don't feel like fixing it now, you can pull stuff up from the database by city tho so that's something

---
+++ Visitor.java
@@ -0,0 +1,81 @@
+//Class that stores info about visitors in an object
+package application;
+
+public class Visitor {
+	private String name;
+	private String email;
+	private String city;
+	private String state;
+	private int zipcode;
+	private String date;
+	private String reason;
+	
+	public Visitor(String name, String email, String city, String state, int zipcode, String date, String reason) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.city = city;
+		this.state = state;
+		this.zipcode = zipcode;
+		this.date = date;
+		this.reason = reason;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public int getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(int zipcode) {
+		this.zipcode = zipcode;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getReason() {
+		return reason;
+	}
+
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+	
+	
+}
