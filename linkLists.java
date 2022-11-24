5ed65117f5b217eb352421e6e5cca2ee8d996148
sonyamaksy <79486998+sonyamaksy@users.noreply.github.com>
2022-11-24 17:54:28 -0600

    Add files via upload

---
+++ linkLists.java
@@ -0,0 +1,138 @@
+/**
+ * Class that initializes the ArrayList and does SQL queries, for now you can only search by city
+ */
+package application;
+
+import java.sql.*;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class linkLists {
+
+	private ObservableList<Visitor> visitors;
+    private static final String INSERT_QUERY = "INSERT INTO visitor_info (name, email, city, state, zipcode,"
+    											+ " date_of_visit, group_amount, reason)"
+    									        + "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+	String cityString = "SELECT name, email, city, state, zipcode, date_of_visit,"
+			+ "group_amount, reason FROM visitor_info WHERE city LIKE ?";
+	
+	String zipString = "SELECT name, email, city, state, zipcode, date_of_visit, "
+			+ "group_amount, reason FROM visitor_info WHERE zipcode LIKE ?";
+	
+	public linkLists() {
+		visitors = FXCollections.observableArrayList();
+	}
+
+	//method to insert values into database
+	public void insertRecord(String name, String email, String city, String state, int zip,
+							 String date, int group_amount, String reason)
+	{
+		try 
+		{
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/guestbook", "root", "");
+			PreparedStatement preparedStatement = con.prepareStatement(INSERT_QUERY);
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, email);
+            preparedStatement.setString(3, city);
+            preparedStatement.setString(4, state);
+            preparedStatement.setInt(5, zip);
+            preparedStatement.setString(6, date);
+            preparedStatement.setInt(7, group_amount);
+            preparedStatement.setString(8, reason);
+            
+            System.out.println(preparedStatement);
+            preparedStatement.executeUpdate();
+		}
+		
+		catch (SQLException e) {
+            // print SQL exception information
+			printSQLException(e);
+        }	
+	}
+	
+	//throwing an error if something in sql went wrong
+	 public static void printSQLException(SQLException ex) 
+	 {
+		 for (Throwable e: ex) 
+		 {
+	            if (e instanceof SQLException) 
+	            {
+	                e.printStackTrace(System.err);
+	                System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+	                System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+	                System.err.println("Message: " + e.getMessage());
+	                Throwable t = ex.getCause();
+	                while (t != null) {
+	                    System.out.println("Cause: " + t);
+	                    t = t.getCause();
+	                }
+	            }
+		 }
+	}   
+	
+	//method for getting the values from database
+	public void displayByCity(String city)
+	{
+		ObservableList<Visitor> newList = FXCollections.observableArrayList();		
+		Connection con = null;
+		try {
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/guestbook", "root", "");
+			
+			
+			PreparedStatement ps = con.prepareStatement(cityString);
+			ps.setString(1, "%" + city + "%");
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				newList.add(new Visitor(rs.getString("name"), rs.getString("email"),
+										rs.getString("city"), rs.getString("state"),
+										rs.getInt("zipcode"), rs.getString("date_of_visit"),
+										rs.getInt("group_amount"), rs.getString("reason")));
+			}
+		}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+		visitors = newList;
+		
+	}
+	
+	public void displayByZip(String zip)
+	{
+		ObservableList<Visitor> newList = FXCollections.observableArrayList();		
+		Connection con = null;
+		try {
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/guestbook", "root", "");
+			
+			
+			PreparedStatement ps = con.prepareStatement(zipString);
+			ps.setString(1, "%" + zip + "%");
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				newList.add(new Visitor(rs.getString("name"), rs.getString("email"),
+										rs.getString("city"), rs.getString("state"),
+										rs.getInt("zipcode"), rs.getString("date_of_visit"),
+										rs.getInt("group_amount"), rs.getString("reason")));
+			}
+		}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+		visitors = newList;
+		
+	}
+	
+	public ObservableList<Visitor> getVisitors() {
+		return visitors;
+	}
+
+	public void setVisitors(ObservableList<Visitor> visitors) {
+		this.visitors = visitors;
+	}
+	
+}
---
+++ sceneController.java
@@ -0,0 +1,217 @@
+//Controller class nothing fancy 
+package application;
+
+import java.io.IOException;
+import java.sql.*;
+import java.time.format.DateTimeFormatter;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.*;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+public class sceneController {
+
+	private linkLists model = new linkLists();
+	
+    @FXML
+    Button switch_database, switch_visitor, submit_button;
+    
+    @FXML
+    private Button search_button;
+    
+    @FXML
+    void handleCity(ActionEvent event)
+    {
+    	handlesearchButton(event);
+    }
+    
+    //searching by city and zipcode 
+    @FXML
+    void handlesearchButton(ActionEvent event) {
+    	setTableView();
+    	Window owner = search_button.getScene().getWindow();
+
+    	if (!city_input.getText().isEmpty() && !zip_input.getText().isEmpty())
+    	{
+    		model.displayByCity(city_input.getText());
+    	}
+    	else if (!city_input.getText().isEmpty())
+    	{
+    		model.displayByCity(city_input.getText());
+    	}
+    	
+    	else if(!zip_input.getText().isEmpty())
+    	{
+    		model.displayByZip(zip_input.getText());
+    	}
+    	
+    	//if no people were found, an error window shows up saying that no people were found
+    	if (model.getVisitors().isEmpty())
+    	{
+    		showAlert(Alert.AlertType.ERROR, owner, "No Entries", "No people were found based on your search.");
+    	}
+    	else
+    	{
+            tableview.setItems(model.getVisitors());
+    	}
+    }
+
+    @FXML
+    void btnSubmitClicked(ActionEvent event) throws SQLException{
+    	Window owner = submit_button.getScene().getWindow();
+    	
+    	//throwing an error if not all fields are filled out
+    	if (fname.getText().isEmpty() || lname.getText().isEmpty() || midinit.getText().isEmpty()
+    		|| city.getText().isEmpty() || state.getText().isEmpty() || email.getText().isEmpty()
+    		|| (date.getValue() == null) || amount_of_people.getText().isEmpty() || 
+    		zip.getText().isEmpty() || reason.getText().isEmpty())
+    	{
+    		showAlert(Alert.AlertType.ERROR, owner, "Form Error", "Please fill out all fields.");
+    		return;
+    	}
+    	
+    	String zip1 = zip.getText();
+    	String amount = amount_of_people.getText();
+    	
+    	String name = fname.getText() + " " + midinit.getText() + ". " + lname.getText();
+    	String email1 = email.getText();
+    	String city1 = city.getText();
+    	String state1 = state.getText();
+    	int zipcode = Integer.parseInt(zip1);
+    	String date1 = date.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+    	int people = Integer.parseInt(amount);
+    	String reason1 = reason.getText();
+    	
+    	model.insertRecord(name, email1, city1, state1, zipcode, date1, people, reason1);
+    	showAlert(Alert.AlertType.CONFIRMATION, owner, "Registration Successful!",
+                "You are registered " + name);
+    	
+    }
+    
+    //method for displaying an error
+    private static void showAlert(Alert.AlertType alertType, Window owner, String title, String message) 
+    {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.initOwner(owner);
+        alert.show();
+    }
+    
+    @FXML
+    void switchToVisitor() throws IOException {
+    	Parent root = FXMLLoader.load(getClass().getResource("visitors_view.fxml"));
+    	Stage window = (Stage) switch_visitor.getScene().getWindow();
+    	window.setScene(new Scene(root));
+    }
+    
+    @FXML
+    void switchToDatabase() throws IOException {
+    	Parent root = FXMLLoader.load(getClass().getResource("database_scene.fxml"));
+    	Stage window = (Stage) switch_database.getScene().getWindow();
+    	window.setScene(new Scene(root));
+    }
+    
+    @FXML
+    public void setTableView() {
+    	nameColumn.setCellValueFactory(new PropertyValueFactory<Visitor, String>("Name"));
+    	emailColumn.setCellValueFactory(new PropertyValueFactory<Visitor, String>("Email"));
+    	cityColumn.setCellValueFactory(new PropertyValueFactory<Visitor, String>("City"));
+    	stateColumn.setCellValueFactory(new PropertyValueFactory<Visitor, String>("State"));
+    	zipColumn.setCellValueFactory(new PropertyValueFactory<Visitor, Integer>("Zipcode"));
+    	dateColumn.setCellValueFactory(new PropertyValueFactory<Visitor, String>("Date"));
+    	peopleColumn.setCellValueFactory(new PropertyValueFactory<Visitor, Integer>("Group"));
+    	reasonColumn.setCellValueFactory(new PropertyValueFactory<Visitor, String>("Reason"));
+    	tableview.setItems(model.getVisitors());
+    }
+    
+ 
+    //values for visitor's side
+	@FXML
+    private TextField amount_of_people;
+
+    @FXML
+    private TextField city;
+
+    @FXML
+    private DatePicker date;
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    private TextField fname;
+
+    @FXML
+    private TextField lname;
+
+    @FXML
+    private TextField midinit;
+
+    @FXML
+    private TextArea reason;
+
+    @FXML
+    private TextField state;
+
+    @FXML
+    private TextField zip;
+    
+    
+    //values for database side
+    @FXML
+    private TextField city_input;
+
+    @FXML
+    private DatePicker date_input;
+
+    @FXML
+    private TextField metro_input;
+
+    @FXML
+    private TextArea reason_input;
+
+    @FXML
+    private TextField zip_input;
+    
+    @FXML
+    public TableView<Visitor> tableview;
+
+    @FXML
+    private TableColumn<Visitor, String> cityColumn;
+
+    @FXML
+    private TableColumn<Visitor, String> dateColumn;
+
+    @FXML
+    private TableColumn<Visitor, String> emailColumn;
+
+    @FXML
+    private TableColumn<Visitor, String> nameColumn;
+
+    @FXML
+    private TableColumn<Visitor, String> reasonColumn;
+
+    @FXML
+    private TableColumn<Visitor, String> stateColumn;
+    
+    @FXML
+    private TableColumn<Visitor, Integer> zipColumn;
+    
+    @FXML
+    private TableColumn<Visitor, Integer> peopleColumn;
+
+}
